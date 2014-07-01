6a12ec806ba14de41127f826826287be9a5094f7
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-07-01 21:02:43 +0200

    diskdump example

--- diskdump/lib/diskdump-example.js
+++ diskdump/lib/diskdump-example.js
@@ -1,6 +1,6 @@
 /*
- * simpleauth-example
- * https://github.com/parroit/simpleauth-example
+ * diskdump-example
+ * https://github.com/parroit/diskdump-example
  *
  * Copyright (c) 2014 Andrea Parodi
  * Licensed under the MIT license.
@@ -10,7 +10,7 @@
 
 var cm = require('connect-mail');
 var smtp = require('cm-smtp');
-var simpleAuth = require('cm-simpleauth');
+var diskdump = require('cm-diskdump');
 
 //create smtp server instance
 var server = smtp({
@@ -21,18 +21,14 @@ var server = smtp({
 //create connect-mail instance
 var app = cm(server);
 
-//use simpleauth for user authentication
-app.authorize(simpleAuth('user-storage/users.json'));
+//use diskdump for user authentication
+app.use(diskdump('./mails'));
 
 //add simple middleware
 app.use(function(mail, next){
-    console.dir(mail);
-    //accept all mails for me
     if (mail.to === 'me@parro.it'){
         mail.accept();
-
     } else { 
-        //rejects all other ones
         mail.reject();
     }
 
